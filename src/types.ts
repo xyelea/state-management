600b7e9d4ffc9c18dc5a10ac46a1721cfaaf2741
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-07-10 16:34:49 +0700

    create pokemon types

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface Pokemon {
+  id: number;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+}
