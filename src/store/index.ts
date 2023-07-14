f46b11158f67f5bef1b67b64c2903d73ad849a66
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-07-14 13:18:16 +0700

    update : pokemonapi reducer and middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./searchSlice";
+import { pokemonApi } from "./pokemonApi";
 
+// Mengkonfigurasi store Redux menggunakan configureStore dari @reduxjs/toolkit
 export const store = configureStore({
   reducer: {
     search: searchReducer,
+    pokemonApi: pokemonApi.reducer,
+  },
+  middleware(getDefaultMiddleware) {
+    // Menggabungkan middleware default dengan middleware dari pokemonApi
+    return getDefaultMiddleware().concat(pokemonApi.middleware);
   },
 });
 
+// Tipe state global dari store
 export type RootState = ReturnType<typeof store.getState>;
+// Tipe dispatch yang digunakan dalam aplikasi
 export type AppDispatch = typeof store.dispatch;
